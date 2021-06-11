c9c02fdf131bb1c049fe0a992a4dfd955f3b9222
Bandyopadhyay <a.l.bandyopadhyay@accenture.com>
2021-06-11 19:28:23 +0530

    pattern

---
+++ src/pattern/Pattern11.java
@@ -0,0 +1,29 @@
+package pattern;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        int n=5;
+        int nst = 1;
+        int nsp = n-1;
+        int row = 1;
+
+        while (row <=n){
+
+            int csp = 1;
+            while(csp<=nsp){
+                System.out.print("  ");
+                csp++;
+            }
+            int cst = 1;
+            while (cst <= nst){
+                System.out.print("*   ");
+                cst++;
+            }
+
+            nsp--;
+            nst++;
+            row++;
+            System.out.println("");
+        }
+    }
+}
